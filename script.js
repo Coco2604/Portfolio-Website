d586f1e73492491dbd544b81a7761a055f1ebd5a
Coco2604 <codebabe21@gmail.com>
2025-09-12 17:30:29 +0530

    final commit ig

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto",{
-    strings: ["Student","Competitive Programmer","Web Developer"],
+    strings: ["Student","Programmer","Software Engineer","Data Scientist"],
     typeSpeed:110,
     backSpeed:60,
     loop:true
